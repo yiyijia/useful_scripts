4088d4e89b1d9c58ae66ef0355ca77dad09f08a9
yiyijia <mysansa52@gmail.com>
2022-12-10 11:14:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-console.log(process.env)
 require('dotenv').config()
 const { main_handler: v2Free } = require('./src/v2FreeAutoCheckIn')
 const { main_handler: GlaDOS } = require('./src/GlaDOSCheckIn')
 const { main_handler: iKuuu } = require('./src/iKuuuCheckIn')
-console.log(process.env)
 const main_handler = async () => {
   await v2Free()
   await GlaDOS()
