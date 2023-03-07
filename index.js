918ab70e00f4edf4e564f28cd0fd06ae664b89ae
yiyijia <mysansa52@gmail.com>
2023-03-07 13:01:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 const { main_handler: v2Free } = require('./src/v2FreeAutoCheckIn')
 const { main_handler: GlaDOS } = require('./src/GlaDOSCheckIn')
 const { main_handler: iKuuu } = require('./src/iKuuuCheckIn')
+const { main_handler: aliyp } = require('./src/aliy_yunpan_sign')
 const main_handler = async () => {
+  await aliyp()
   await v2Free()
   await GlaDOS()
-  await iKuuu()
+  //await iKuuu()
 }
 main_handler();
